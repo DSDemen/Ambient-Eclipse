fd39d14890d089ef667b1040dadd1a66a07fe13b
DomaJa <demenzopen@gmail.com>
2024-05-04 17:26:14 +0300

    Update

--- Versions/0.20.3/Ambient-Eclipse/scripts/Ambient-Eclipse.lua
+++ Versions/0.20.3/Ambient-Eclipse/scripts/Ambient-Eclipse.lua
@@ -1,5 +1,11 @@
 local matrix = load_script('Ambient-Eclipse:scripts/a_findpath.lua')
 local oper = load_script('Ambient-Eclipse:scripts/p_opera.lua')
 local pID = 1
+local defstate = 0
 
 
+function on_interact(x, y, z, pID)
+
+    oper.openhud(Cheat)
+    
+end
